940ac6e2f9afb05a9ec1384728f907e1a63defb7
Yves P <havokinspiration@gmail.com>
2017-08-02 13:25:34 +0200

    Add copyright file-docblocks

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
- * @link          http://github.com/elephfront/robo-css-minify
+ * @link          http://github.com/elephfront/robo-sass
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 define('TESTS_ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
